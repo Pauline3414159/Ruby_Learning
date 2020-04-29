23600e4ad7c4077ca5686dee436d09d4b8d7c859
Pauline3414159 <fagie27@hotmail.com>
2020-04-29 19:47:59 +0000

    rubocop fixes on the area calculator

--- small_ex/calc_area.rb
+++ small_ex/calc_area.rb
@@ -15,17 +15,15 @@ def print_to_user(string)
 end
 
 def meters_or_feet
-  print_to_user("Enter 'm' for meters or 'f' for feet")
   ans = nil
   loop do
     ans = gets.chomp.downcase
     break if ans == 'm' || ans == 'f'
     print_to_user('please enter m or f')
   end
-  if ans == 'm'
-    'meters'
-  else
-    'feet'
+  case ans
+  when 'm' then 'meters'
+  when 'f' then 'feet'
   end
 end
 
@@ -41,34 +39,44 @@ def get_width(uflag)
   width
 end
 
-def calc_area (length , width)
+def calc_area(length, width)
   area = (length * width).round(2)
   area
 end
 
-def display_area_results(areaans,uflag)
-  case uflag
-  when 'meters'
-    print_to_user("The area is #{areaans} square #{uflag}")
-    print_to_user("The area is #{(areaans * SQ_METER_TO_SQ_FEET).round(2)} square feet")
-    print_to_user("The area is #{(areaans * SQ_METER_TO_SQ_INCH).round(2)} square inches")
-    print_to_user("The are is #{(areaans * SQ_METER_TO_SQ_CM).round(2)} square centimeters")
-  when 'feet'
-    print_to_user("The area is #{areaans} square #{uflag}")
-    print_to_user("The area is #{(areaans * SQ_FEET_TO_SQ_METERS).round(2)} square meters")
-    print_to_user("The area is #{(areaans * SQ_FEET_TO_SQ_INCH).round(2)} square inches")
-    print_to_user("The area is #{(areaans * SQ_FEET_TO_SQ_CM).round(2)} square centimeters")
-  end
+def print_area_result_m(areaans)
+  print_to_user("The area is #{areaans} square m ")
+  print_to_user("The area is #{(areaans * SQ_METER_TO_SQ_FEET).round(2)}
+  square ft")
+  print_to_user("The area is #{(areaans * SQ_METER_TO_SQ_INCH).round(2)}
+  square inch")
+  print_to_user("The are is #{(areaans * SQ_METER_TO_SQ_CM).round(2)}
+  square cm")
+end
+
+def print_area_result_ft(areaans)
+  print_to_user("The area is #{areaans} square ft")
+  print_to_user("The area is #{(areaans * SQ_FEET_TO_SQ_METERS).round(2)}
+  square m")
+  print_to_user("The area is #{(areaans * SQ_FEET_TO_SQ_INCH).round(2)}
+  square inch")
+  print_to_user("The area is #{(areaans * SQ_FEET_TO_SQ_CM).round(2)}
+  square cm")
 end
 
 print_to_user('Welcome to room area calcuator')
 
 loop do
+  print_to_user("Enter 'm' for meters or 'f' for feet")
   units_flag = meters_or_feet
-  length = get_length (units_flag)
-  width = get_width (units_flag)
-  area = calc_area(length , width)
-  display_area_results(area, units_flag)
+  length = get_length(units_flag)
+  width = get_width(units_flag)
+  area = calc_area(length, width)
+  if units_flag == 'meters'
+    print_area_result_m(area)
+  elsif units_flag == 'feet'
+    print_area_result_ft(area)
+  end
   print_to_user("Enter 'y' to calculate the room area again")
   ans = gets.chomp.downcase
   break unless ans == 'y'
